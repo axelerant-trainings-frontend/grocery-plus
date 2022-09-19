5bab02f5fa233c86e3a36495d98120c0dfc867f2
Ummar Hamid <kaxama34@gmail.com>
2022-09-19 20:11:00 +0530

    Setup Jest with React Testing Library

---
+++ components/button/Button.test.js
@@ -0,0 +1,16 @@
+import { render, screen } from "@testing-library/react";
+import Button from "./Button";
+import "@testing-library/jest-dom";
+
+describe("Test Button component", () => {
+  it("renders the component", () => {
+    render(<Button>Test data</Button>);
+    const button = screen.getByRole("button");
+    expect(button).toBeInTheDocument();
+  });
+  it("renders the correct child data", () => {
+    render(<Button>Test data</Button>);
+    const button = screen.getByRole("button");
+    expect(button).toHaveTextContent(/test data/i);
+  });
+});
