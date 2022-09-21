1182e85fa8611ebc95352f717aaf1e1f8bcca9ed
Ummar Hamid <kaxama34@gmail.com>
2022-09-21 13:46:47 +0530

    Install and Setup contentful

---
+++ utils/contentfulClient.ts
@@ -0,0 +1,27 @@
+import { createClient, Entry, FieldItem } from "contentful";
+
+const contentfulClient = () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || "",
+    accessToken: process.env.CONTENTFUL_ACCESS_TOKEN || "",
+    host: process.env.CONTENTFUL_HOST,
+  });
+  const getProducts = async () => {
+    try {
+      const entries = await client.getEntries({
+        content_type: "product",
+      });
+      const sanitizedEntries = entries.items.map(
+        (item: Entry<FieldItem> | any) => {
+          return { ...item.fields, id: item.sys.id };
+        }
+      );
+      return sanitizedEntries;
+    } catch (error) {
+      console.log(`Error fetching Products: ${error}`);
+    }
+  };
+  return { getProducts };
+};
+
+export default contentfulClient;
