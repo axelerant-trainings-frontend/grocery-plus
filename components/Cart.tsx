d950793bb7e78a3db8822049dbc6bf27c0e34630
subhasishnathAxelerant <subhasish.nath@axelerant.com>
2022-09-22 10:01:57 +0530

    FTP-1356: Modification of cartSlice and Cart component.

--- components/Cart.tsx
+++ components/Cart.tsx
@@ -1,16 +1,51 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
-import { selectCart, add, remove } from "../redux/reduxSlices/cartSlice";
+import {
+  selectCart,
+  addToCart,
+  removeFromCart,
+} from "../redux/reduxSlices/cartSlice";
 const Cart = () => {
   const cartItemCount = useSelector(selectCart);
   const dispatch = useDispatch();
   return (
-    <div>
-      Cart Page
-      <button onClick={() => dispatch(add())}>+</button>
-      {cartItemCount}
-      <button onClick={() => dispatch(remove())}>-</button>
-    </div>
+    <>
+      <h1 className="mb-3 text-xl">Our Cart</h1>
+      <div className="w-1/3 flex mb-4">
+        <button
+          onClick={() =>
+            dispatch(addToCart({ id: 3, title: "Face Wash", count: 1 }))
+          }
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-1/2"
+        >
+          Add Face Wash
+        </button>
+        <button
+          onClick={() => dispatch(removeFromCart({ id: 3, title: "Face wash" }))}
+          className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded-full w-1/2 ml-1"
+        >
+          Remove Face Wash
+        </button>
+      </div>
+
+      <ul className="w-1/3 flex mb-4 flex-col">
+        {cartItemCount.map((cartItem, index) => {
+          return (
+            <li key={index} className="flex py-6 border-2 border-gray-200">
+              <div className="flex-1 py-6 px-4 sm:px-6">
+                <h3>Item: </h3>
+                <p>{cartItem["title"]}</p>
+              </div>
+              <div className="flex-1 py-6 px-4 sm:px-6">
+                <h3>Quantity:</h3>
+                <p>{cartItem["count"]}</p>
+              </div>
+            </li>
+          );
+        })}
+      </ul>
+
+    </>
   );
 };
 
--- redux/reduxSlices/cartSlice.ts
+++ redux/reduxSlices/cartSlice.ts
@@ -1,26 +1,46 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 export interface CartState {
-  value: number;
+  cart: Array<{ id: number; title: string; count: number }>;
 }
 
 const initialState: CartState = {
-  value: 12,
+  cart: [
+    { id: 1, title: "Soap", count: 1 },
+    { id: 2, title: "shampoo", count: 1 },
+  ],
 };
 
 export const cartSlice = createSlice({
   name: "cart",
   initialState,
   reducers: {
-    add: (state) => {
-      state.value += 1;
+    addToCart: (state, action) => {
+      const itemInCart = state.cart.find((cartItem) => {
+        return cartItem.id === action.payload.id;
+      });
+      if (itemInCart) {
+        itemInCart.count++;
+      } else {
+        state.cart.push({ ...action.payload });
+      }
     },
-    remove: (state) => {
-      state.value -= 1;
+    removeFromCart: (state, action) => {
+      const itemInCart = state.cart.find((cartItem) => {
+        return cartItem.id === action.payload.id;
+      });
+      if (itemInCart) {
+        if (itemInCart.count > 1) {
+          itemInCart.count--;
+        } else {
+          state.cart.pop();
+        }
+      }
     },
   },
 });
 
-export const selectCart = (state) => state.cart.value;
-export const { add, remove } = cartSlice.actions;
+export const selectCart = (state) =>
+  state.cart.cart.map((cartitem) => cartitem);
+export const { addToCart, removeFromCart } = cartSlice.actions;
 export default cartSlice.reducer;
