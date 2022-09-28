b43e5cfc26c212a10a295262b652091c37a7c75d
tushar patel <tushar.patel@axelerant.com>
2022-09-29 02:36:51 +0530

    FTP-1378:design-system-tokens-colors

---
+++ components/colors/Colors.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+function Colors (props) {
+  const { children, ...rest} = props
+  return (
+     <div className = {``}{...rest}>{children}</div>
+  )
+}
+
+export default Colors;
