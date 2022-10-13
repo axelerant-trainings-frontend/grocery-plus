e7ccde9536a97b65bf21896368bd29df0c31cbd4
Bassam Ismail <2114712+skippednote@users.noreply.github.com>
2022-10-13 10:27:07 +0530

    Update Time.test.tsx

--- components/time/Time.test.tsx
+++ components/time/Time.test.tsx
@@ -3,16 +3,17 @@ import Time from '../time/Time';
 
 describe('Test Time component', () => {
   it('renders the component and checks for the specific class', () => {
-    const StartTime = '10PM';
-    const EndTime = '10AM';
-    render(<Time start={StartTime} end={EndTime} variant="active" />);
+    const startTime = '10PM';
+    const endTime = '10AM';
+    render(<Time start={startTime} end={endTime} variant="active" />);
     const time = screen.getByRole('button');
     expect(time).toHaveClass('text-red-primary');
   });
+  
   it('renders the component and checks for the specific class', () => {
-    const StartTime = '10PM';
-    const EndTime = '10AM';
-    render(<Time start={StartTime} end={EndTime} variant="inactive" />);
+    const startTime = '10PM';
+    const endTime = '10AM';
+    render(<Time start={startTime} end={endTime} variant="inactive" />);
     const time = screen.getByRole('button');
     expect(time).toHaveClass('text-charcoal');
   });
